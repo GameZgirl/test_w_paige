c9c8ab95cdf60d55622e9e8f0809670dcb0eec4f
pmb41 <pmb41@duke.edu>
2024-03-25 17:40:02 -0700

    first attempt at awesome method

--- test_file.py
+++ test_file.py
@@ -2,3 +2,21 @@ import math
 
 def placeholder():
     pass
+
+# Get user input
+user_input = input("Enter a number: ")
+
+while(True): 
+    # Convert input to an integer
+    try:
+        number = int(user_input)
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        exit()
+
+    # Check if the number is even
+    if number % 2 == 0:
+        print("I love you")
+        break
+    else:
+        print("Please type an even number.")
